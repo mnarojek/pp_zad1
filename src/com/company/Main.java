16af68b64f8d1e0653e5130a9a5074266451aea5
Mateusz Narojek <mateusz.narojek@gmail.com>
2020-05-11 16:30:44 +0200

    Tworzenie klasy i dodanie pola

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+class PeopleStat{
+
+}
+
+class Person{
+    private final String firstName;
+    private final String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
